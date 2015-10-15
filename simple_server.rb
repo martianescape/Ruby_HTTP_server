5e20757833854ca3f246bb79cb04295204a1639c
Revanth P <revanth.p.revu@gmail.com>
2015-10-15 12:28:09 +0530

    adds request_line_handler and request_header_handler

--- simple_server.rb
+++ simple_server.rb
@@ -1,22 +1,41 @@
 require 'socket'
 
-server = TCPServer.new('127.0.0.1', 2000)
 
+
+def request_line_handler(line, client)
+  array = line.split(' ')
+  method, resource, version = array
+  client.puts "method is #{method}"
+  client.puts "resource is #{resource}"
+  client.puts "version is #{version}"
+end
+
+def request_header_handler(line, client)
+  client.gets
+  array = line.split("\n")
+  header_hash = {}
+  array.each do
+    |headerline|
+    header_hash[headerline.split(":")[0]] = headerline.split(":")[1][1..-1]
+  end
+    client.puts "hhash is #{header_hash}"
+end
+
+server = TCPServer.new('127.0.0.1', 2000)
 while true
   client = server.accept #wait for a client to connect
   client.puts "Server is connected at #{Time.now}"
   client.puts "enter the file containing the http request"
   filename = client.gets.chomp
-  puts "Http request is in #{filename}"
   file = open(filename, 'r')
   line = request_line = file.readline
+  request_line_handler(request_line, client)
   client.puts "request_line = #{request_line}"
   request_header = "" 
   while line[0] != "\n"
     line = file.readline
     request_header += line
   end
-  client.puts "request_headers = #{request_header}"
-  
+  request_header_handler(request_header, client)
   client.close
 end
